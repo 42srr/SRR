1eda45f5c2f2f10076dd879be30a5db497d03fc2
joonwan <joonwan1024@naver.com>
2024-12-02 02:32:10 +0900

    feat: initialize project

---
+++ src/test/java/com/ggs/srr/SrrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ggs.srr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
